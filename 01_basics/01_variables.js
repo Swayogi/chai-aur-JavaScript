1e0e5d8cfd7ac3be5c2708ce393d62d3101308d6
Swayogi <mrswayogi@gmail.com>
2024-09-30 22:14:06 +0000

    Variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123456
+let accountEmail = "swayogi@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+
+console.log(accountId);
+
+accountEmail = "new"
+accountPassword = "new"
+accountCity = "new"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*
+ Don't Use var  
+
+*/
+
+console.log(accountState);
+
