1d545c9c5bc148ffb56e1368055963318e96dd74
Swayogi <mrswayogi@gmail.com>
2024-11-10 05:59:23 +0000

    Datatypesummary

---
+++ 01_basics/05_DataType-Summary.js
@@ -0,0 +1,36 @@
+// # Primitive datatype
+
+// 7 types : String , Number , Boolean , null , Undefined , Symbol , BigInt
+
+// const  Scorce = false;
+// const  numScorce = 100.3;
+// const isLoggedIn = false;
+// const outSideTemp = null
+// let userEmail;
+
+// //symbol 
+// const id = Symbol('123')
+// const newid = Symbol('123')
+// console.log(id == newid); //false
+ 
+// const bignum = 13215425423126343n //bigint
+
+
+//Reference (Non Primitive)
+ 
+// Array , Objects , Functions 
+
+// const heros = ["a","b","c"];
+// console.log(heros)
+
+// let myObj ={
+//     name:"somu",
+//     age:22,
+// }
+const myFunction = function() {
+    
+    console.log("HEllo Word");
+    
+}
+
+
