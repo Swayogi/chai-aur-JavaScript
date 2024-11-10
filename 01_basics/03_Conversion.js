7aa6044295314f52964833bccb374c5e7b5a65e5
Swayogi <mrswayogi@gmail.com>
2024-11-10 04:00:22 +0000

    Conversions

---
+++ 01_basics/03_Conversion.js
@@ -0,0 +1,21 @@
+let score="33"
+
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+
+let newscore="33asd"
+
+console.log(newscore + "  "+ typeof newscore);
+
+
+//"33"  33
+//"33abs" NaN
+//true  1 and false  0
+
+
+// 1 is true 
+// 0 is false 
+// "somu" is true
+// "" is false 
