3ced5465bf9dcdaf429c3b1631a4c461a7085b26
Swayogi <mrswayogi@gmail.com>
2024-11-10 04:20:43 +0000

    Operations

--- 01_basics/03_Conversion.js
+++ 01_basics/03_Conversion.js
@@ -1,13 +1,13 @@
-let score="33"
+// let score="33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
 
-let newscore="33asd"
+// let newscore="33asd"
 
-console.log(newscore + "  "+ typeof newscore);
+// console.log(newscore + "  "+ typeof newscore);
 
 
 //"33"  33
@@ -19,3 +19,51 @@ console.log(newscore + "  "+ typeof newscore);
 // 0 is false 
 // "somu" is true
 // "" is false 
+
+
+//*********************************/Operations 
+//  let value =3
+//  let negValue = -value
+//  console.log(negValue);
+
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2/2);
+//  console.log(2*2);
+//  console.log(2%2);
+//  console.log(2**2);
+
+// let str1="Hello"
+// let str2=" Somu"
+// let str3=str1+str2
+// console.log(str3);
+
+// //complex
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log("1"+2+2); // all in string 
+// console.log(1+2+"2"); //3 + string 😂
+
+// console.log(+true); //1
+// console.log(+""); //1 insted of zero 
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2
+
+let gamecounter=10
+console.log(gamecounter);
+++gamecounter; //prefix 
+console.log(gamecounter);
+gamecounter++; //postfix 
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
