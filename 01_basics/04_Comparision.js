1d25939baf79f9b608d0d1ac5ad117e0c42ba405
Swayogi <mrswayogi@gmail.com>
2024-11-10 04:36:58 +0000

    Comparision

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,28 @@
+//Basic
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//Different type string and number 
+// console.log("2">1);
+// console.log("02">1);
+
+//Null 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+
+//Undefined 
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined != 0);
+
+
+// ===
+console.log("2" === 2 );
+
+
+
