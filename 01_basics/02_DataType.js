a47dc3c5926994d58d3d6703a2d27f94e35997a5
Swayogi <mrswayogi@gmail.com>
2024-11-10 03:45:14 +0000

    datatypes

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello"); //we are using nodejs , not browser
+
+console.log(3+3); console.log("you");
+
+
+let name="somu"
+let age=18
+let isLoginedIn=false
+
+
+
+
+
+
+//number => 2 to power 53
+//bigint
+//string '' ro ""
+//boolean  true or false
+//null  standalone value
+//undefined 
+//symbol  unique
+//object
+
+console.log(typeof "somu");
+
+console.log(typeof undefined); //Undefined
+console.log(typeof null);   //Object
